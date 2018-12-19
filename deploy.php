15de7a1399672ef9ddcc93176a4df3160ad4184c
Fendo181 <fendo181git@gmail.com>
2018-12-19 10:52:56 +0900

    add deployer.php

---
+++ deploy.php
@@ -0,0 +1,39 @@
+<?php
+namespace Deployer;
+
+require 'recipe/laravel.php';
+// phpdotenvを呼びすようにする
+with(new \Dotenv\Dotenv(__DIR__))->load();
+
+set('application', env('APP_NAME'));
+set('repository', '{Your Repository Name}');
+set('branch', '{Branch Name}');
+set('git_tty', false);
+
+add('shared_files', ['.env']);
+add('shared_dirs', []);
+add('writable_dirs', ['bootstrap/cache', 'storage']);
+
+host(env('DEPLOYER_MC_HOST'))
+    ->stage('production')
+    ->user(env('DEPLOYER_MC_USER'))
+    ->port(env('DEPLOYER_MC_PORT'))
+    ->identityFile('~/.ssh/id_rsa.pub')
+    ->set('deploy_path', '/var/www/');
+
+task('build', function () {
+    run('cd {{release_path}} && build');
+});
+
+// .envをアップロードする
+task('upload:env', function () {
+    upload('.env', '{{deploy_path}}/shared/.env');
+})->desc('.envをアップロード');
+
+before('deploy:shared','upload:env');
+
+// [Optional] if deploy fails automatically unlock.
+after('deploy:failed', 'deploy:unlock');
+
+// Migrate database before symlink new release.
+before('deploy:symlink', 'artisan:migrate');
